c7f7365e68185895e7cb432a60d4dcaa7ea340b0
AshanOdi <ashanuni25@gmail.com>
2025-09-02 23:59:09 +0530

    Add Unit component and update App component to handle dynamic data submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import Unit from './Components/Unit';
 
 
 const App = () => {
@@ -8,6 +9,10 @@ const App = () => {
     const [name,setName] = useState("");
     const [city,setCity] = useState("");
     const [position,setPosition] = useState("");
+
+    const [myData,setMyData] = useState([]);
+
+    console.log(myData);
 
   return( 
   <div className="main_container">
@@ -30,26 +35,34 @@ const App = () => {
         }}/>
         <button className="submit_btn" onClick={() =>
             {
-                console.log({
-                imgUrl,
-                name,
-                city,
-                position
+                
+            setMyData((pre)=>{
+                return [...pre, {
+                image:imgUrl,
+                name:name, 
+                city:city,
+                position:position
+                }];
             })
+
             setImgUrl((pre)=>{
                 if (pre.length > 0) {
                     return ""; }
                 else { return pre; }
             })
 
-            setName((pre)=>{
-                (pre.length > 0 ? "" : pre); 
-            });
+            setName((pre)=>(pre.length > 0) ? "" : pre);
+            setCity((pre)=>(pre.length > 0) ? "" : pre);
+            setPosition((pre)=>(pre.length > 0) ? "" : pre);
         }
         }>Submit</button>
     </div>
     <div className="main_Right">
-
+        {
+            myData?.map( (data,index) => {
+                return <Unit key={index} img={data.image} name={data.name} city={data.city} position={data.position} />
+            })
+        }
     </div>
 
   </div> 
---
+++ src/Components/Unit.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Unit = ({img,name,city,position}) => {
+  return (
+    <div className='unit_container'>
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
+      <p><span>{city}</span>
+      <span>{position}</span>
+      </p>
+    </div>
+    
+
+  )
+}
+
+export default Unit
