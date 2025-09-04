62c787d69c5c333dad86c5e36c7e13041e82812f
AshanOdi <ashanuni25@gmail.com>
2025-09-04 17:25:08 +0530

    Implement routing in AppRoute component and remove routing from App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Contact from "./Components/Contact";
-import ContactDetails from "./Components/ContactDetails";
-import User from "./Components/User";
-
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/contact/details" element={<ContactDetails />} />
-        <Route path="/contact/:id" element={<User />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div></div>;
 };
 
 export default App;
